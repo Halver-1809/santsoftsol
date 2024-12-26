8100351e2dac4a1439238eeb675f6d15fe42acfe
halver <h.verdugo18@gmail.com>
2024-12-26 18:32:29 -0500

    component auht

--- src/routes/authRoutes.tsx
+++ src/routes/authRoutes.tsx
@@ -1,31 +1,17 @@
 import React from 'react';
-import { Icon } from '@chakra-ui/react';
-import { MdLock } from 'react-icons/md';
+
 import { useRoutes } from 'react-router-dom';
 
-import SignInCentered from '@/views/auth/signIn';
 import ErrorComponent from '@/views/error';
 
 const authRoutes: RoutesType[] = [
-  {
-    name: 'Iniciar sesión',
-    layout: '/auth',
-    path: '/sign-in',
-    icon: <Icon as={MdLock} width="20px" height="20px" color="inherit" />,
-    component: <SignInCentered />,
-  },
   {
     name: 'Error',
     layout: '/auth',
     path: '*',
     component: <ErrorComponent />,
   },
-/*   {
-    name: 'hometwo',
-    layout: '/auth',
-    path: '/hometwoo',
-    component: <HomeTwo />,
-  } */
+
 ];
 
 console.log('AuthRoutes Rendered', authRoutes);
